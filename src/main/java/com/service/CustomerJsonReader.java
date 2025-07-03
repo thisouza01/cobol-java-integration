3203c8a9e4f5bb796e0e360cae1bfd88979feb71
thisouza01 <tihsouza01@gmail.com>
2025-07-02 22:52:56 -0300

    Class returning a List.

--- src/main/java/com/service/CustomerJsonReader.java
+++ src/main/java/com/service/CustomerJsonReader.java
@@ -4,22 +4,33 @@ import org.json.JSONObject;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerJsonReader {
 
-    public CustomerJsonReader(String filePath){
+    public List<String> JsonReader(String filePath){
         try {
             String jsonString = new String(Files.readAllBytes(Paths.get(filePath)));
 
             JSONObject jsonObject = new JSONObject(jsonString);
 
             JSONArray jsonArray = jsonObject.getJSONArray("customer");
+            // Gera uma lista
+            List<String> customerList = new ArrayList<>();
+
             for (int i = 0; i < jsonArray.length(); i++){
                 JSONObject object = jsonArray.getJSONObject(i);
+
+                customerList.add(object.toString());
+
                 System.out.println("Customer: " + object.toString());
             }
+
+            return customerList;
         } catch (Exception e) {
             System.err.println("Erro ao ler o JSON: " + e.getMessage());
         }
+        return null;
     }
 }
