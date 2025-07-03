3951b3fe11f3908a37d3cb146e6880c8a458ded4
thisouza01 <tihsouza01@gmail.com>
2025-07-02 22:53:37 -0300

    Testing class CustomerJsonReader.

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,13 +2,17 @@ package main.java.com;
 
 import main.java.com.service.CustomerJsonReader;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        // Le Arquivo JSON
+        // Le Arquivo JSON e retorna uma lista
         // Apontar posteriormente para o local que o COBOL irá criar o arquivo JSON.
         String filePath = "C:\\Users\\tihso\\IdeaProjects\\cobol-java-integration\\src\\customers.json";
-        CustomerJsonReader readJSON = new CustomerJsonReader(filePath);
+        // instancio a classe
+        CustomerJsonReader readJSON = new CustomerJsonReader();
 
-
+        // chama o metodo e armazena o resultado em uma lista
+        List<String> resultedList = readJSON.JsonReader(filePath);
     }
 }
