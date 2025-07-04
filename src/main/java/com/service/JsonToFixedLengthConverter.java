37dd080116ba830902ec5eadfcffbd4d26d18c53
Thiago <tihsouza01@gmai.com>
2025-07-04 17:23:21 -0300

    Implementing Json to fixed length record.

--- src/main/java/com/service/JsonToFixedLengthConverter.java
+++ src/main/java/com/service/JsonToFixedLengthConverter.java
@@ -1,12 +1,28 @@
 package main.java.com.service;
 
+import main.java.com.model.Customer;
+
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
-/*public class JsonToFixedLengthConverter() {
-    public void fixedLengthConverter(String filePath){
-        try (BufferedWriter writer = ){
+public class JsonToFixedLengthConverter {
 
+    public void fixedLengthConverter(List<Customer> customers, String filePath)throws IOException{
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))){
+            for (Customer customer : customers){
+                String line = formatRecord(customer);
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing file: " + e.getMessage());
         }
     }
+
+    private static String formatRecord(Customer customer){
+        return String.format("%-4s%-20s%010.2f",
+                customer.getId(),
+                customer.getName(),
+                customer.getBalance());
+    }
 }
-*/
